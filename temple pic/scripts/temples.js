9bda76b5ef774ae192414fa676a46ec00fca6fae
Rigfen <Rigfen@gmail.com>
2026-03-11 21:47:49 +0000

    change my large and small css and got js to work with the footer

--- temple pic/scripts/temples.js
+++ temple pic/scripts/temples.js
@@ -1,5 +1,12 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+});
+
 const year = new Date().getFullYear();
 document.getElementById("currentyear").textContent = year;
 
 document.getElementById("lastModified").textContent =
-    "Last Modified: " + document.lastModified;
+"Last Modification: " + document.lastModified;
