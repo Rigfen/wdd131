932e36907503ec359ac54fdba19db19bc7e42eb6
Rigfen <Rigfen@gmail.com>
2026-03-12 23:06:55 +0000

    asdf

---
+++ scripts/ebert.js
@@ -0,0 +1,6 @@
+const ebert = document.querySelector("#ebert");
+const video = document.querySelector("#ebertVideo");
+
+ebert.addEventListener("click", function () {
+    video.style.display = "block";
+});
